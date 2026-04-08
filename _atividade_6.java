298a485c3cd3db2ed260872f3f2986a125becb89
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-08 16:57:06 -0300

    Create _atividade_6.java

---
+++ _atividade_6.java
@@ -0,0 +1,29 @@
+package Java_Lista_1_If_e_Se;
+
+import java.util.Scanner;
+
+public class _atividade_6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int idade;
+
+        System.out.println("Qual é a sua idade?");
+        idade = sc.nextInt();
+
+        while (idade < 0 || idade > 120) {
+            System.out.println("Idade inválida, digite outra.");
+            idade = sc.nextInt();
+        }
+
+        if (idade <= 12) {
+            System.out.println("Você é uma criança (idade muito infantil).");
+        } else if (idade <= 17) {
+            System.out.println("Você é um adolescente.");
+        } else if (idade <= 64) {
+            System.out.println("Você é um adulto.");
+        }
+        else {
+            System.out.println("Você é um idoso.");
+        }
+    }
+}
