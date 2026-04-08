11eb732de461cdedaa04677d76c01e29f7a8602e
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-08 16:30:14 -0300

    Create _atividade_4.java

---
+++ _atividade_4.java
@@ -0,0 +1,26 @@
+package Java_Lista_1_If_e_Se;
+
+import java.util.Scanner;
+
+public class _atividade_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int idade;
+
+        System.out.println("Qual é a sua idade?");
+        idade = sc.nextInt();
+
+        while (idade < 0) {
+            System.out.println("Idade inválida, digite outra.");
+            idade = sc.nextInt();
+        }
+
+        if (idade >= 18) {
+            System.out.println("Você é maior de idade");
+        }
+        else {
+            System.out.println("Você é menor de idade");
+        }
+        sc.close();
+    }
+}
