10842cd971e98cce25365717f1df26532215cfbd
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-08 16:04:24 -0300

    Create _atividade_2.java

---
+++ _atividade_2.java
@@ -0,0 +1,21 @@
+package Java_Lista_1_If_e_Se;
+
+import java.util.Scanner;
+
+public class _atividade_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double numb, result;
+
+        System.out.println("Digite o valor do produto para ver se ele tem um desconto");
+        numb = sc.nextDouble();
+
+        if (numb > 100 ) {
+            System.out.printf("O preço original é: R$%.2f%n", numb);
+        }
+        else {
+            result = (numb * 0.90);
+            System.out.printf("O preço final com desconto é: R$%.2f%n", result);
+        }
+    }
+}
