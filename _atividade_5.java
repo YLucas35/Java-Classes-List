c3f926f3d2b889f7c6f99f54ec8ae86e06a4c26e
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-08 16:37:01 -0300

    Create _atividade_5.java

---
+++ _atividade_5.java
@@ -0,0 +1,26 @@
+package Java_Lista_1_If_e_Se;
+
+import java.util.Scanner;
+
+public class _atividade_5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int idade;
+
+        System.out.println("Qual é a sua idade?");
+        idade = sc.nextInt();
+
+        while (idade < 0) {
+            System.out.println("Idade inválida, digite outra.");
+            idade = sc.nextInt();
+        }
+
+        if (idade >= 16) {
+            System.out.println("Você pode votar.");
+        }
+        else {
+            System.out.println("Você não pode votar.");
+        }
+        sc.close();
+    }
+}
