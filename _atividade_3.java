e76b6b4b3a482a8848a29786b2b9ae7cd93cd14f
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-08 16:13:51 -0300

    Create _atividade_3.java

---
+++ _atividade_3.java
@@ -0,0 +1,20 @@
+package Java_Lista_1_If_e_Se;
+
+import java.util.Scanner;
+
+public class _atividade_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int temp;
+
+        System.out.println("Quantos graus está a temperatura no ambiente?");
+        temp = sc.nextInt();
+
+        if (temp > 40) {
+            System.out.println("Temperatura extremamente alta");
+        }
+        else {
+            System.out.println("Temperatura dentro dos limites normais");
+        }
+    }
+}
