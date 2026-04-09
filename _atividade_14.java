e0e7230cc997f1a117501a8723e8f1497f609efe
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-09 17:53:47 -0300

    Create _atividade_14.java

---
+++ _atividade_14.java
@@ -0,0 +1,20 @@
+package Java_Lista_1_If_e_Se;
+
+import java.util.Scanner;
+
+public class _atividade_14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numb;
+
+        System.out.println("Insira a primeira nota do aluno");
+        numb = sc.nextInt();
+
+        if (numb >= 7) {
+            System.out.println("Ele está aprovado");
+        }
+        else {
+            System.out.println("Ele está reprovado");
+        }
+    }
+}
