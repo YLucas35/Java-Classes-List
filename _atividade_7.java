cee8e9f8fe3f6be1d03091f1ba73bf208fe01444
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-09 17:40:26 -0300

    Update _atividade_7.java

--- _atividade_7.java
+++ _atividade_7.java
@@ -5,40 +5,28 @@ import java.util.Scanner;
 public class _atividade_7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int numb1, numb2, numb3;
-        boolean result1, result2; //As variáveis são só tamplates para fazer o cálculo, elas não tem nenhum valor atribuído a elas.
 
-        System.out.println("Insira o primeiro número");
-        numb1 = sc.nextInt();
+        System.out.print("Insira o primeiro número: ");
+        int numb1 = sc.nextInt();
 
-        System.out.println("Insira o segundo número");
-        numb2 = sc.nextInt();
+        System.out.print("Insira o segundo número: ");
+        int numb2 = sc.nextInt();
 
-        System.out.println("Insira o terceiro número");
-        numb3 = sc.nextInt();
+        System.out.print("Insira o terceiro número: ");
+        int numb3 = sc.nextInt();
 
-        if (numb1 > numb2) {
-            result1 = (numb1 >= numb3);
-            System.out.println(numb1+" é o maior de todos");
-            sc.close();
-        } else if (numb2 > numb1) {
-            if (numb2 >= numb3) {
-                System.out.println(numb2+" é o maior de todos");
-                sc.close();
-            }
-            else {
-                System.out.println(numb3+" é o maior de todos");
-                sc.close();
-            }
-        } else {
-            if (numb3 >= numb1) {
-                result2 = (numb3 > numb2);
-                System.out.println(numb3+" é o maior de todos");
-                sc.close();
-            }
-            else {
-                System.out.println(numb1+" é o maior de todos");
-            }
+        int maior = numb1;
+
+        if (numb2 > maior) {
+            maior = numb2;
         }
+
+        if (numb3 > maior) {
+            maior = numb3;
+        }
+
+        System.out.println(maior + " é o maior de todos.");
+
+        sc.close();
     }
 }
